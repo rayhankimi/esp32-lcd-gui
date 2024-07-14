88f750aa5ffcc30f5dbe284d30b1ba20037d6b4b
Rayhan K <166932170+rayhankimi@users.noreply.github.com>
2024-07-15 06:55:36 +0800

    adding hcsr04, implement refresh button that still not working

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
+import dht
 from machine import Pin, SoftI2C
 from time import sleep as delay
 from esp32_i2c_lcd import I2cLcd
+from hcsr04 import HCSR04
 
-# Setup LCD
 lcdI2C = SoftI2C(scl=Pin(22), sda=Pin(21), freq=100000)
 lcd = I2cLcd(lcdI2C, 39, 2, 16)
 
+dht_sensor = dht.DHT22(Pin(33))
+hcsr04_sensor = HCSR04(trigger_pin=5, echo_pin=18, echo_timeout_us=10000)
+
+
 class Button:
-    def __init__(self, button_pin1, button_pin2):
+
+    def __init__(self, button_pin1, button_pin2, refresh_pin):
         self.button_pins = [Pin(button_pin1, Pin.IN), Pin(button_pin2, Pin.IN)]
+        self.refresh_pin = Pin(refresh_pin, Pin.IN)
         self.state = 0
         self.previous_val = [0, 0]
-    
+        self.previous_refresh = 0
+
+
     def val(self):
         return [pin.value() for pin in self.button_pins]
 
+
     def debounce(self):
         current_val = self.val()
-        delay(0.05)  # Debounce delay
+        delay(0.01)  # Debounce delay
         delayed_val = self.val()
 
         if current_val == delayed_val:
@@ -25,11 +35,13 @@ class Button:
         
         return self.previous_val
 
+
     def handle(self):
         current_val = self.debounce()
+        refresh_val = self.refresh_pin.value()
 
         if current_val[0] == 1 and self.previous_val[0] == 0: 
-            if self.state < 4:
+            if self.state < 2:
                 self.state += 1
 
         if current_val[1] == 1 and self.previous_val[1] == 0: 
@@ -38,19 +50,80 @@ class Button:
 
         self.previous_val = current_val
 
+        if refresh_val == 1 and self.previous_refresh == 0:
+            self.previous_refresh = refresh_val
+            return True
+
+        self.previous_refresh = refresh_val
+        return False
+
+
+class Interaction:
+    def __init__(self, select):
+        self.select = select
+
+
+    def read_temp(self):
+        dht_sensor.measure()
+        temp = dht_sensor.temperature()
+        return temp
+
+
+    def read_distance(self):
+        distance = hcsr04_sensor.distance_cm()
+        return distance
+
+
+    def display_temp(self):
+        temp = self.read_temp()
+        lcd.clear()
+        lcd.putstr("Temp : {:.1f} C".format(temp))
+        lcd.move_to(0, 1)
+        lcd.putstr("     Distance v")
+
+
+    def display_distance(self):
+        distance = self.read_distance()
+        lcd.clear()
+        lcd.putstr("Distance : {:.1f} Cm".format(distance))
+        lcd.move_to(0, 1)
+        lcd.putstr("^ Temp    LED v")
+
+
+    def display_control(self):
+        lcd.clear()
+        lcd.putstr("Not Implmntd yet")
+        lcd.move_to(0, 1)
+        lcd.putstr("^ Distance")
+
+
+    def decide(self):
+        if self.select == 2:
+            self.display_temp()
+        
+        elif self.select == 1:
+            self.display_distance()
+
+        else:
+            self.display_control()
+
+
+
 def main():
-    buttons = Button(12, 27)
+    buttons = Button(12, 27, 13)  
+    interaction = Interaction(0)  
     last_state = -1 
 
     while True:
-        buttons.handle()
+        refresh = buttons.handle()
 
-        if buttons.state != last_state:
-            lcd.clear()
-            lcd.putstr(f"State: {buttons.state}")
+        if buttons.state != last_state or refresh:
+            interaction.select = buttons.state  
+            interaction.decide()  
             last_state = buttons.state
 
-        delay(0.01)  # Main loop delay
+        delay(0.1)  
+
 
 if __name__ == '__main__':
     main()
